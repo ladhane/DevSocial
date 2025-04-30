9f6d20e8091016b37a18892b873a5189aaa41876
Mayuri Ladhane <mayuriladhane3@gmail.com>
2025-04-30 16:39:59 +0530

    sanitizing our API's

--- src/index.js
+++ src/index.js
@@ -7,25 +7,115 @@ const app = express();
 
 app.use(express.json())
 app.use('/admin', authUser)
-app.get('/admin/getAllData',(req,res)=>{
-    res.send([{fname: 'John', lname: 'Doe'},{fname: 'Jane', lname: 'Doe'}])
-})
-app.post('/admin/addData',(req,res)=>{
-    res.send('Data added successfully')
-})
-app.delete('/admin/deleteData',(req,res)=>{
-    res.send('Data deleted successfully')
+
+
+//get feed
+app.get('/feed',async(req,res)=>{
+    try{
+        const users = await User.find({});
+        if(!users){
+            return res.status(404).send('No users found')
+        }
+        res.send(users);
+    }catch(err){
+        res.status(500).send('Error fetching users:', err);
+    }
 })
 
 app.post('/signup',async (req,res)=>{
     const user = new User(req.body)
+    const allowedFields = ['firstName','lastName','email','password']
+    const isValidUser = Object.keys(req.body).every(key => allowedFields.includes(key));
+    if(!isValidUser){
+        return res.status(400).send('Invalid user data')
+    }
     try{
-        await user.save();
-        res.send('User added successfully')
+        const data = await user.save();      
+        res.send(data)
     }catch(err){
-        res.status(500).send('Error adding user:', err);
+        res.status(500).send('Error adding user:'+ err.message);
     }
 })
+
+//Get user by email
+app.get('/user',async(req,res)=>{
+    if(!req.body?.email){
+        return res.status(400).send('Email is required')
+    }
+    try{
+        const user = await User.findOne({email: req.body?.email})
+        if(!user){
+            return res.status(404).send('User with given email not found')
+        }
+        res.send(user);
+    }catch(err){
+        res.status(404).send('user not found')
+    }
+})
+
+//update user based on email
+app.patch('/user/:email',async(req,res)=>{
+    const userEmail = req.params?.email;
+    if(!userEmail){
+        return res.status(400).send('Email is required')
+    }
+    const allowedFields = ['firstName','lastName','gender','age','photoUrl','about','skills']
+    const isValidUpdate = Object.keys(req.body).every(key => allowedFields.includes(key));
+    if(!isValidUpdate){
+        return res.status(400).send('Invalid update fields')
+    }
+
+    try{
+        const user = await User.findOneAndUpdate({email: userEmail},req.body,{returnDocument: 'after', runValidators: true})
+        if(!user){
+            return res.status(404).send('User with given email not found')
+        }
+        res.send(user)
+    }catch(err){
+        res.status(404).send('ERROR updating data' + err.message)
+    }
+})
+
+// //post new user
+// app.post('/user',async(req,res)=>{
+//     const user = req.body;
+//     try{
+//         const insertedUser = await User.create(user);
+//         res.status(200).send(insertedUser)
+//     }catch(err){
+//         res.status(500).send('Error adding data'+ err.message)
+//     }
+// })
+
+
+//get user by id
+app.delete('/user',async(req,res)=>{   
+    console.log(req.body)
+    if(!req.body?.id){
+        return res.status(400).send('ID is required')
+    }
+    try{
+        // const user = await User.findByIdAndDelete(req.body.id)
+        const user = await User.findByIdAndDelete({_id: req.body?.id})
+        if(!user){
+            return res.status(404).send('User with given ID not found')
+        }
+        res.send('User deleted successfully');
+    }catch(err){
+        res.status(404).send('user not found')
+    }
+})
+
+//update user based on ID
+// app.patch('/user',async(req,res)=>{
+//     const userId = req.body.id
+//     try{
+//         const user = await User.findByIdAndUpdate(userId,req.body,{returnDocument: 'after'})
+//         res.send(user)
+//     }catch(err){
+//         res.status(404).send('User Not Found!!')
+//     }
+// })
 
 connectDB().then(() => {
     console.log('MongoDB connected successfully');
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,65 @@
+const validate = require('validator');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        minLength: 6,
+        required: true,
     },
     lastName: {
-        type: String
+        type: String,
+        minLength: 6,
+        required: true,
     },
     email: {
         type: String,
+        lowerCase: true,
+        required: true,
+        unique: true,
+        validate(value) {
+            if (!validate.isEmail(value)) {
+                throw new Error('Email is not valid')
+            }
+        }
     },
     password: {
-        type: String
+        type: String,
+        minLength: 8,
+        maxLength: 15,
+        required: true,
     },
-    city: {
-        type: String
+    gender: {
+        type: String,
+        validate(value){
+            if(!['male','female','other'].includes(value)){
+                throw new Error('Gender data is not correct')
+            }
+        }
     },
-    mobileNumber: {
-        type: String
+    age: {
+        type: Number,
+        minLength: 18
     },
-})
+    photoUrl: {
+        type: String,
+        validate(value){
+            if(!validate.isURL(value)){
+                throw new Error('Photo URL is not valid')
+            }
+        }
+    },
+    about:{
+        type: String,
+        default: 'user has no description'
+    },
+    skills: {
+        type: [String]
+    }
+},
+{
+    timestamps: true
+});
 
 const User = mongoose.model('User', userSchema);
 
